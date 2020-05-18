24fa6be1cf5067e47549ca4f0ae3fa996f63d42a
Frederico Bortoloti <fredericobortoloti@gmail.com>
2020-05-18 03:15:59 -0300

    Sensibility analysis

--- main_isskdekmeans.py
+++ main_isskdekmeans.py
@@ -61,11 +61,10 @@ datasets = numpy.array([['ionosphere']])
 accs = numpy.zeros((datasets.shape[0], 15))
 ks = numpy.zeros((datasets.shape[0], 4))
 times = numpy.zeros((datasets.shape[0], 15))
-di = 0
 # noinspection PyUnboundLocalVariable
-for ds in datasets:
+sa_flag = True
+for di, ds in datasets:
     if ds == 'lenses' or ds == 'zoo' or ds == 'digit1': # or ds == 'cleveland' or ds == 'led7digit' or ds == 'newthyroid':
-       di = di+1
        continue
     print('\nBase %s\n' % ds)
     # read data samples
@@ -446,6 +445,11 @@ for ds in datasets:
     print('kmss total online time = %.3f s' % timeopt)
     print('accepted examples = %d of %d' % (uacpt_count, m_u))
 
+    if sa_flag:
+        accs[di, 11] = error_kms
+        accs[di, 12] = error_kmss
+        continue
+
     iselect = numpy.nonzero(uselect)[0]
     data_su = data_u[iselect, :]
     labels_su = ucls_v[iselect]
@@ -780,10 +784,7 @@ for ds in datasets:
     accs[di, 12] = error_kmss
     accs[di, 13] = error_kms2
     accs[di, 14] = error_kmss2
-    di = di+1
 
 print('Dataset\tAcc lp\tAcc mf\tAcc mfo\tAcc lpp\tAcc lppo\tAcc elm\tAcc elmo\tAcc sgd\tAcc sgdo\tAcc gnb\tAcc gnbo\tAcc_kms\tAcc kmss\tAcc osgwr\tAcc ossgwr\tk_lpp\tk_lppo\tk_kms\tk_kmss\tk_osgwr\tk_ossgwr\tt lp\tt lpp\tt lppo\tt sgd\tt sgdo\tt gnb\tt gnbo\tt_kms\tt_kmss\tt_osgwr\tt_ossgwr\n')
-di = 0
-for ds in datasets:
+for di, ds in datasets:
     print('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%d\t%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f' % (ds, accs[di, 0], accs[di, 1], accs[di, 2], accs[di, 3], accs[di, 4], accs[di, 5], accs[di, 6], accs[di, 7], accs[di, 8], accs[di, 9], accs[di, 10], accs[di, 11], accs[di, 12], accs[di, 13], accs[di, 14], ks[di, 0], ks[di, 1], ks[di, 2], ks[di, 3], times[di, 0], times[di, 1], times[di, 2], times[di, 3], times[di, 4], times[di, 5], times[di, 6], times[di, 7], times[di, 8], times[di, 9], times[di, 10], times[di, 11], times[di, 12], times[di, 13], times[di, 14]))
-    di = di+1
