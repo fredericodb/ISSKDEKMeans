7b8bc4d45a4e028f510210c97295f147959226d2
Frederico Bortoloti <fredericobortoloti@gmail.com>
2020-05-19 06:39:59 -0300

    Parameters

--- main_isskdekmeans.py
+++ main_isskdekmeans.py
@@ -316,12 +316,12 @@ for ds in datasets:
     plugs = [True]
     kde_kernel = ['gaussian']
 
-    varnk = True
+    varnk = False
     vardt = False
     varnit = False
-    varthd = False
-    varalpha = False
-    varmo = True
+    varthd = True
+    varalpha = True
+    varmo = False
     varplug = False
     varkdekernel = False
 
