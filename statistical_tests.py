00d2574ec3a395fa20586f1686fcdec8a615a723
Frederico Bortoloti <fredericobortoloti@gmail.com>
2020-06-04 01:09:22 -0300

    statistical tests

--- statistical_tests.py
+++ statistical_tests.py
@@ -15,7 +15,7 @@ res = []
 for j in range(7):
     res.append(np.asarray([float(skm_res[i][j]) for i in range(16)]))
 ref = np.asarray([float(skm_res[i][7]) for i in range(16)])
-
+print('Wilcoxon for SKM x all')
 for i in range(0, 7):
     print('skm x %s' % algs[i])
     # compare samples
@@ -37,7 +37,7 @@ res = []
 for j in range(7):
     res.append(np.asarray([float(skm_res[i][j]) for i in range(16)]))
 ref = np.asarray([float(skm_res[i][7]) for i in range(16)])
-
+print('Wilcoxon for SSIKM x all')
 for i in range(0, 7):
     print('skm x %s' % algs[i])
     # compare samples
@@ -63,7 +63,7 @@ skm_res.remove(algs)
 res = []
 for j in range(13):
     res.append(np.asarray([float(skm_res[i][j]) for i in range(16)]))
-
+print('Friedman for all x all')
 # compare samples
 stat, p = friedmanchisquare(res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7], res[8], res[9], res[10],
                             res[11], res[12])
