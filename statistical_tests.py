7855268109a608963009f5cd83131829c095e252
Frederico Bortoloti <fredericobortoloti@gmail.com>
2020-06-04 00:44:56 -0300

    statistical tests

--- statistical_tests.py
+++ statistical_tests.py
@@ -10,14 +10,14 @@ seed(1)
 skm_res = list(csv.reader(open('skm_results.txt', 'r'), delimiter='\t'))
 algs = skm_res[0]
 skm_res.remove(algs)
-lp = skm_res[1:, 0]
-mf = skm_res[1:, 1]
-lpp = skm_res[1:, 2]
-elm = skm_res[1:, 3]
-sgd = skm_res[1:, 4]
-gnb = skm_res[1:, 5]
-osgwr = skm_res[1:, 6]
-skm = skm_res[1:, 7]
+lp = [skm_res[i, 0] for i in range(16)]
+mf = [skm_res[i, 1] for i in range(16)]
+lpp = [skm_res[i, 2] for i in range(16)]
+elm = [skm_res[i, 3] for i in range(16)]
+sgd = [skm_res[i, 4] for i in range(16)]
+gnb = [skm_res[i, 5] for i in range(16)]
+osgwr = [skm_res[i, 6] for i in range(16)]
+skm = [skm_res[i, 7] for i in range(16)]
 
 for i in range(0, 7):
     print('skm x %s', algs[i])
