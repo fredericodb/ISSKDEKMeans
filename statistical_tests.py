fea625837fd9dc9e1cf91bb749b435741ca39579
Frederico Bortoloti <fredericobortoloti@gmail.com>
2020-06-04 00:55:45 -0300

    statistical tests

--- statistical_tests.py
+++ statistical_tests.py
@@ -10,19 +10,20 @@ seed(1)
 skm_res = list(csv.reader(open('skm_results.txt', 'r'), delimiter='\t'))
 algs = skm_res[0]
 skm_res.remove(algs)
-lp = np.asarray([float(skm_res[i][0]) for i in range(16)])
-mf = np.asarray([float(skm_res[i][1]) for i in range(16)])
-lpp = np.asarray([float(skm_res[i][2]) for i in range(16)])
-elm = np.asarray([float(skm_res[i][3]) for i in range(16)])
-sgd = np.asarray([float(skm_res[i][4]) for i in range(16)])
-gnb = np.asarray([float(skm_res[i][5]) for i in range(16)])
-osgwr = np.asarray([float(skm_res[i][6]) for i in range(16)])
-skm = np.asarray([float(skm_res[i][7]) for i in range(16)])
+res = []
+res.append(np.asarray([float(skm_res[i][0]) for i in range(16)]))
+res.append(np.asarray([float(skm_res[i][1]) for i in range(16)]))
+res.append(np.asarray([float(skm_res[i][2]) for i in range(16)]))
+res.append(np.asarray([float(skm_res[i][3]) for i in range(16)]))
+res.append(np.asarray([float(skm_res[i][4]) for i in range(16)]))
+res.append(np.asarray([float(skm_res[i][5]) for i in range(16)]))
+res.append(np.asarray([float(skm_res[i][6]) for i in range(16)]))
+ref = np.asarray([float(skm_res[i][7]) for i in range(16)])
 
 for i in range(0, 7):
     print('skm x %s' % algs[i])
     # compare samples
-    stat, p = wilcoxon(skm, lp)
+    stat, p = wilcoxon(ref, res[i])
     print('Statistics=%.3f, p=%.3f' % (stat, p))
     # interpret
     alpha = 0.05
