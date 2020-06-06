26c0bd8d30d8e3b0512b056443cdcf6f2848c8ce
Frederico Bortoloti <fredericobortoloti@gmail.com>
2020-06-06 16:36:01 -0300

    accuracy per labeled examples

--- isskdekmeans.py
+++ isskdekmeans.py
@@ -372,11 +372,7 @@ class ISSKDEKMeans(BaseEstimator):
             if self.use_kde:
                 if self.kde_clusterbetter[nclstr[i]]:  # previously if self.kde_better:
                     xi = X[i, :]
-                    py = self.kde_clusterclass[nclstr[i]].predict(xi)
-                    try:
-                        output[i] = self.kde_clusterclass[nclstr[i]].predict(xi)
-                    except ValueError:
-                        print("hey")
+                    output[i] = self.kde_clusterclass[nclstr[i]].predict(xi)
 
                 # # vote for class
             # cand = numpy.zeros((3,))
